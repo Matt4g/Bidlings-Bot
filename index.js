bdf9585ba0fb9265ec308038b35918c2412a5711
Matt Gladura <mglad776@gmail.com>
2022-04-29 12:38:12 -0400

    Updated

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 const rwClient = require("./twitterClient.js");
 const CronJob = require("cron").CronJob;
 const fs = require("fs");
-
-const tweet = async () => {
 let bidlingName;
 let bidlingNumber; 
+const tweet = async () => {
 try {
-        bidlingNumber = (Math.ceil(Math.random() * 1000))
         fs.readFile("bidlings.txt", "utf-8", (err, data) =>{
-            if(err){
-                console.error(err)
-                return
-            }
+           bidlingNumber = (Math.ceil(Math.random() * 666))
             bidlingName = data.split("\r\n")[bidlingNumber]
-            console.log(bidlingName)
         })
-        await rwClient.v2.tweet( "Bidling #"+ bidlingNumber +": "+ bidlingName )
-        
+        await rwClient.v2.tweet( "Bidling #"+ (bidlingNumber + 1) +": "+ bidlingName )
+        console.log(bidlingName)
     } catch (e) {
         console.error(e)
     }
 }
 
-const job = new CronJob("0 8 * * *", () => {
+const job = new CronJob(" 0 8 * * *", () => {
     tweet()
 })
 
