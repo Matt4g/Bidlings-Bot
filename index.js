090a2ab68943449e1146aef9d3937a49a366c729
Matt Gladura <75256340+Matt4g@users.noreply.github.com>
2022-05-12 12:16:07 -0400

    Added the last bidlings

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let bidlingNumber;
 const tweet = async () => {
 try {
         fs.readFile("bidlings.txt", "utf-8", (err, data) =>{
-           bidlingNumber = (Math.ceil(Math.random() * 737))
+           bidlingNumber = (Math.ceil(Math.random() * 998))
             bidlingName = data.split("\r\n")[bidlingNumber]
         })
         await rwClient.v2.tweet( "Bidling #"+ (bidlingNumber + 1) +": "+ bidlingName )
@@ -15,13 +15,10 @@ try {
         console.error(e)
     }
 }
-
 const job = new CronJob(" 0 8 * * *", () => {
     tweet()
 })
-
 job.start();
-
 /*
 pm2 list
 pm2 start index.js
--- twitterClient.js
+++ twitterClient.js
@@ -1,12 +1,9 @@
 const {TwitterApi} = require("twitter-api-v2");
-
 const client = new TwitterApi({
     appKey:"Umu9cqFYpH07aFmsKZZgNcHBD",
     appSecret:"ZYMfXMwQyGoFxqJt7LMZi14fmMd4My5Q1tBJy0YOZ09GdXoeGP",
     accessToken:"1519687276917407744-DxGeej0R373zjjxfKBf38PNWET9Jd6",
     accessSecret:"9nZCVHS7dC9E8Eh5lg7Mq0YEVJF3L5E0x8THEtvkOrnTM"
 })
-
 const rwClient = client.readWrite
-
 module.exports = rwClient
